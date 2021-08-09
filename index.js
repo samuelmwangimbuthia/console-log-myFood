71a9ba4a8b8a1efa4d3999541768cf78fa4d164f
Samuel <mbuthia.jsamuel@gmail.com>
2021-08-09 16:55:02 +0300

    using generators in place of custom iterators

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ readline.on("line", async (line) => {
   switch (line.trim()) {
     case "list vegan foods":
       {
-        axios.get("http://localhost:3000/food").then(({ data }) => {
-          let idx = 0;
-          const veganOnly = data.filter((food) =>
-            food.dietary_preferences.includes("vegan")
-          );
+        //using a generator in place of custom iterator
+       const {data} = await axios.get("http://localhost:3000/food");
+       function *listVeganFoods(){
+        let idx = 0;
+        const veganOnly = data.filter((food) =>
+          food.dietary_preferences.includes("vegan")
+        );
+        //pass a food out the iterator using the yield keyword
+        while(veganOnly[idx]) {
+          yield veganOnly[idx];
+          idx++;
+        }
+       }
+     
+          /*
           const veganIterable = {
             [Symbol.iterator]() {
               return {
@@ -34,11 +44,11 @@ readline.on("line", async (line) => {
               };
             },
           };
-          for (let val of veganIterable) {
+          */
+          for (let val of listVeganFoods()) {
             console.log(val.name);
           }
           readline.prompt();
-        });
       }
       break;
     case "log":
@@ -46,6 +56,17 @@ readline.on("line", async (line) => {
       const it = data[Symbol.iterator]();
       let actionIt;
 
+      // yield between the functionalities
+      // custom return and throw methods already defined in generator functions
+      function *actionGenerator(){
+        //food entered by the user is passed to the generator first
+        const food = yield;
+        //prompt the user to enter the serving size
+        const servingSize = yield askForServingSize();
+        yield displayCalories(servingSize,food);
+      }
+      /*
+        //cycle through the actions array
       const actionIterator = {
         [Symbol.iterator]() {
           let positions = [...this.actions];
@@ -76,15 +97,15 @@ readline.on("line", async (line) => {
         },
         actions: [askForServingSize, displayCalories],
       };
-
-      function askForServingSize(food) {
+    */
+      function askForServingSize() {
         readline.question(
           `How many servings did you eat? (as a decimal: 1, 0.5, 1.244, etc..)`,
           (servingSize) => {
             if (servingSize === "neverMind" || servingSize === "n") {
               actionIt.return();
             } else {
-              actionIt.next(servingSize, food);
+              actionIt.next(servingSize);
             }
           }
         );
@@ -130,7 +151,9 @@ readline.on("line", async (line) => {
           const food = position.value.name;
           if (food === item) {
             console.log(`${item} has ${position.value.calories} calories`);
-            actionIt = actionIterator[Symbol.iterator]();
+            //actionIt = actionIterator[Symbol.iterator]();
+            actionIt = actionGenerator();
+            actionIt.next();
             actionIt.next(position.value);
           }
           position = it.next();
